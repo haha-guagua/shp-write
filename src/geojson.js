2c65697fdb6ec96898ec1b987d3e3c80dbc55c5f
Robert Claypool <robert.claypool@fielda.com>
2021-09-07 23:49:01 -0500

    Wrap LineString & MultiLineString coordinate pairs (#5)
    With this fix, LineString & MultiLineString GeoJSON features won't have a NULL geometry in the resulting polyline shapefile. Polygon shapefiles seem to be working fine, so this doesn't change how Polygon or MultiPolygon GeoJSON is parsed. See https://github.com/mapbox/shp-write/issues/44

--- src/geojson.js
+++ src/geojson.js
@@ -5,10 +5,22 @@ module.exports.polygon = justType('Polygon', 'POLYGON');
 function justType(type, TYPE) {
   return function(gj) {
     var oftype = gj.features.filter(isType(type));
+
+    var geometries;
+    if (TYPE === 'POLYLINE') {
+      // Wrap LineString & MultiLineString coordinate pairs.
+      // https://github.com/mapbox/shp-write/issues/44
+      geometries = oftype.map(function (t) {
+        return [justCoords(t)];
+      });
+    } else {
+      geometries = oftype.map(justCoords);
+    }
+
     return {
-      geometries: oftype.map(justCoords),
+      geometries: geometries,
       properties: oftype.map(justProps),
-      type: TYPE
+      type: TYPE,
     };
   };
 }
